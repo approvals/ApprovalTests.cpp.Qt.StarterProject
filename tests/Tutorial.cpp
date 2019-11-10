8fb81c67f93e1b5e45c8de8f34c005f67c9e20e6
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-11-10 20:57:57 +0000

    c Add TODO to update Tutorial

--- tests/Tutorial.cpp
+++ tests/Tutorial.cpp
@@ -1,6 +1,7 @@
-#include "ApprovalTests.hpp"
+#include "ApprovalTestsQt.hpp"
 #include "Catch.hpp"
 
+// TODO Update this to ApprovalTests.cpp.Qt
 // See the tutorial at:
 //   https://github.com/approvals/ApprovalTests.cpp/blob/master/doc/Tutorial.md#top
 
